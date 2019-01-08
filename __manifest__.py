5dbfd77945d8d3f015d134dc1bbefeaef07446e4
root <root@localhost.localdomain>
2019-01-08 17:54:24 +0100

    Basic Skeleton for Odoo Application

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """Manage Trainings""",
+
+    'description': """Open Academy module for managing trainings:
+                    -training courses
+                    -trainings sessions
+                    -attendees registrations
+                        """,
+
+    'author': "Emeka Osigwe",
+    'website': "http://www.yedc.com.ng",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/openacademy.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
